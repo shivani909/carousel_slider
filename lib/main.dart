88a86cdcf1104ef207f9a0371cac6f2aab6b7bb5
shivani909 <shivanipandeycontact@gmail.com>
2023-01-11 19:34:16 +0530

    animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:animation_parallex/controllers/data_provider.dart';
+
+
 import 'package:animation_parallex/screens/main_screen.dart';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'package:video_player/video_player.dart';
 
@@ -18,13 +22,16 @@ class ParallaxApp extends StatefulWidget {
 class _ParallaxAppState extends State<ParallaxApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (context) => DataProvider(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Catalogue(),
       ),
-      home: Catalogue(),
     );
   }
 }
--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -39,8 +39,10 @@ class _CatalogueState extends State<Catalogue> with TickerProviderStateMixin {
   String description = '';
   List _controllers = [];
   late AnimationController animationController;
-  late Animation<TextStyle> animation;
-  final Tween<double> _fontSizeTween = Tween(begin: 16.0, end: 32.0);
+  CarouselController _carouselController = CarouselController();
+  // late Animation<TextStyle> animation;
+  // final Tween<double> _fontSizeTween = Tween(begin: 16.0, end: 32.0);
+
   @override
   void initState() {
     _controller0 = VideoPlayerController.network(_products[0].videoUrl)
@@ -53,101 +55,175 @@ class _CatalogueState extends State<Catalogue> with TickerProviderStateMixin {
       ..initialize();
     _controllers.add(_controller2);
 
-    setState(() {});
+    // setState(() {});
 
-    animationController = AnimationController(
-        vsync: this, duration: const Duration(milliseconds: 1000));
+    // animationController = AnimationController(
+    //     vsync: this, duration: const Duration(milliseconds: 1000));
 
-    animation = TextStyleTween(
-      begin: const TextStyle(
-        fontSize: 16,
-        color: Colors.black,
-        fontWeight: FontWeight.bold,
-      ),
-      end: const TextStyle(
-        fontSize: 32,
-        color: Colors.black,
-        fontWeight: FontWeight.bold,
-      ),
-    ).animate(animationController);
+    // animation = TextStyleTween(
+    //   begin: const TextStyle(
+    //     fontSize: 16,
+    //     color: Colors.black,
+    //     fontWeight: FontWeight.bold,
+    //   ),
+    //   end: const TextStyle(
+    //     fontSize: 32,
+    //     color: Colors.black,
+    //     fontWeight: FontWeight.bold,
+    //   ),
+    // ).animate(animationController);
 
-    animation.addListener(() {
-      setState(() {});
-    });
-    animationController.forward();
+    // animation.addListener(() {
+    //   setState(() {});
+    // });
+    // animationController.forward();
     super.initState();
   }
 
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    animationController.dispose();
-    super.dispose();
-  }
+  // @override
+  // void dispose() {
+  //   animationController.dispose();
+  //   super.dispose();
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(child: Container()),
-          CarouselSlider.builder(
-            itemCount: _controllers.length,
-            options: CarouselOptions(
-              height: 600,
-              aspectRatio: 16 / 9,
-              viewportFraction: 0.8,
-              initialPage: 0,
-              enableInfiniteScroll: true,
-              scrollDirection: Axis.horizontal,
-              enlargeFactor: 0.3,
-              onPageChanged: (i, carouselPageChangedReason) {
-                if (i == 0) {
-                  _controller0.play();
-                } else if (i == 1) {
-                  _controller1.play();
-                } else if (i == 2) {
-                  _controller2.play();
-                } else {
-                  _controller0.pause();
-                  _controller1.pause();
-                  _controller2.pause();
-                }
-                setState(() {
-                  text = _products[i].name;
-                  description = _products[i].description;
-                });
-              },
-            ),
-            itemBuilder: (context, index, i) {
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Expanded(
-                      child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: VideoPlayer(_controllers[index]),
-                  )),
-                  ShowUpAnimation(
-                      delayStart: Duration(milliseconds: 500),
-                      animationDuration: Duration(milliseconds: 500),
-                      curve: Curves.easeInSine,
-                      direction: Direction.vertical,
-                      offset: -0.5,
-                      child: Column(children: [
-                        ListTile(
-                          title: Padding(
-                            padding: EdgeInsets.symmetric(vertical: 10),
-                            child: Text(text)),
-                          subtitle: Text(description),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(left: 42.0, top: 42, bottom: 42),
+                child: Text(
+                  'Make it personal',
+                  style: TextStyle(
+                    letterSpacing: 0.5,
+                    fontSize: 18,
+                    fontFamily: 'Montas',
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+              ),
+              CarouselSlider.builder(
+                itemCount: _controllers.length,
+                carouselController: _carouselController,
+                options: CarouselOptions(
+                  height: MediaQuery.of(context).size.height * 0.8,
+                  aspectRatio: 16 / 9,
+                  viewportFraction: 0.85,
+                  initialPage: 0,
+                  enableInfiniteScroll: true,
+                  scrollDirection: Axis.horizontal,
+                  enlargeFactor: 0.3,
+                  onPageChanged: (i, carouselPageChangedReason) {
+                    if (i == 0) {
+                      _controller0.play();
+                    } else if (i == 1) {
+                      _controller1.play();
+                    } else if (i == 2) {
+                      _controller2.play();
+                    } else {
+                      _controller0.pause();
+                      _controller1.pause();
+                      _controller2.pause();
+                    }
+                    setState(() {
+                      text = _products[i].name;
+                      description = _products[i].description;
+                    });
+                  },
+                ),
+                itemBuilder: (context, index, i) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.55,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: VideoPlayer(_controllers[index]),
                         ),
-                      ]))
-                ],
-              );
-            },
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 30.0, right: 20),
+                        child: ShowUpAnimation(
+                            delayStart: const Duration(milliseconds: 500),
+                            animationDuration: const Duration(milliseconds: 500),
+                            curve: Curves.easeInOut,
+                            direction: Direction.vertical,
+                            offset: -0.5,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  text,
+                                  style: const TextStyle(
+                                    letterSpacing: 0.5,
+                                    height: 2,
+                                    fontSize: 26,
+                                    fontFamily: 'Montas',
+                                    fontWeight: FontWeight.w300,
+                                  ),
+                                ),
+                                const SizedBox(height: 10),
+                                Text(
+                                  description,
+                                  style: const TextStyle(
+                                    letterSpacing: 0.8,
+                                    fontSize: 15,
+                                    height: 1.3,
+                                    fontFamily: 'Montas',
+                                    fontWeight: FontWeight.w200,
+                                  ),
+                                ),
+                              ],
+                            )),
+                      ),
+                    ],
+                  );
+                },
+              ),
+        
+              // Padding(
+              //   padding: const EdgeInsets.only(left: 36.0, right: 20),
+              //   child: ShowUpAnimation(
+              //       delayStart: const Duration(milliseconds: 500),
+              //       animationDuration: const Duration(milliseconds: 500),
+              //       curve: Curves.easeInOut,
+              //       direction: Direction.vertical,
+              //       offset: -0.5,
+              //       child: Column(
+              //         crossAxisAlignment: CrossAxisAlignment.start,
+              //         children: [
+              //           Text(
+              //             text,
+              //             style: const TextStyle(
+              //               letterSpacing: 0.5,
+              //               height: 2,
+              //               fontSize: 26,
+              //               fontFamily: 'Montas',
+              //               fontWeight: FontWeight.w300,
+              //             ),
+              //           ),
+              //           const SizedBox(height: 10),
+              //           Text(
+              //             description,
+              //             style: const TextStyle(
+              //               letterSpacing: 0.8,
+              //               fontSize: 18,
+              //               height: 1.3,
+              //               fontFamily: 'Montas',
+              //               fontWeight: FontWeight.w200,
+              //             ),
+              //           ),
+              //         ],
+              //       )),
+              // ),
+            ],
           ),
-          Expanded(child: Container()),
-        ],
+        ),
       ),
     );
   }
