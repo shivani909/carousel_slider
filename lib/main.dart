67c21a29d91960dea10d7a0b2164700775c98107
shivani909 <shivanipandeycontact@gmail.com>
2023-01-11 19:35:24 +0530

    animation changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:animation_parallex/controllers/data_provider.dart';
 
 
 import 'package:animation_parallex/screens/main_screen.dart';
@@ -22,16 +21,13 @@ class ParallaxApp extends StatefulWidget {
 class _ParallaxAppState extends State<ParallaxApp> {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => DataProvider(),
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: Catalogue(),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: Catalogue(),
     );
   }
 }
